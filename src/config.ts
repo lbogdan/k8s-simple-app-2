23c3105aae77768c61fd3196459fc2b48730c795
Bogdan Luca <luca.bogdan@gmail.com>
2022-10-24 11:07:16 +0000

    Add config watching and reloading.

--- src/config.ts
+++ src/config.ts
@@ -1,19 +1,27 @@
 import { readFile } from 'fs/promises';
+import chokidar from 'chokidar';
 
 export const config: Record<string, string> = {
   environment: 'development',
 };
 
-try {
-  const newConfig: Record<string, string> = JSON.parse(
-    await readFile('config/config.json', { encoding: 'utf8' })
-  );
-  for (const key of Object.keys(newConfig)) {
-    const value = newConfig[key];
-    if (value) {
-      config[key] = value;
+async function loadConfig() {
+  console.debug('[DEBUG] loading config');
+  try {
+    const newConfig: Record<string, string> = JSON.parse(
+      await readFile('config/config.json', { encoding: 'utf8' })
+    );
+    for (const key of Object.keys(newConfig)) {
+      const value = newConfig[key];
+      if (value) {
+        config[key] = value;
+      }
     }
+  } catch (err) {
+    // ignore
   }
-} catch (err) {
-  // ignore
 }
+
+loadConfig();
+
+chokidar.watch('config/config.json').on('change', loadConfig);
